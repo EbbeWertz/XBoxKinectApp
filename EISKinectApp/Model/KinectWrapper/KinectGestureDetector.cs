907c6ca58ed68b026637a0d624cb95f3e0f50262
RobinLambregts <robin.lambregts@student.uhasselt.be>
2025-11-12 10:48:02 +0100

    GameView

--- EISKinectApp/Model/KinectWrapper/KinectGestureDetector.cs
+++ EISKinectApp/Model/KinectWrapper/KinectGestureDetector.cs
@@ -20,5 +20,75 @@ namespace EISKinectApp.Model.KinectWrapper
             var rightHand = skeleton.GetFrontViewInMeters(JointType.HandRight);
             return leftHand.Y <= head.Y - 0.15f && rightHand.Y <= head.Y + 0.15f;
         }
+        
+        // -----------------------------
+        // Linkerarm omhoog \_
+        // -----------------------------
+        public static bool LeftArmUp(KinectSkeleton skeleton)
+        {
+            var shoulder = skeleton.GetFrontViewInMeters(JointType.ShoulderLeft);
+            var elbow = skeleton.GetFrontViewInMeters(JointType.ElbowLeft);
+            var hand = skeleton.GetFrontViewInMeters(JointType.HandLeft);
+            
+            return hand.Y > elbow.Y + 0.3 && elbow.Y < shoulder.Y + 0.1 && elbow.Y > shoulder.Y - 0.1;
+        }
+
+        // -----------------------------
+        // Linkerarm opzij __
+        // -----------------------------
+        public static bool LeftArmSide(KinectSkeleton skeleton)
+        {
+            var shoulder = skeleton.GetFrontViewInMeters(JointType.ShoulderLeft);
+            var hand = skeleton.GetFrontViewInMeters(JointType.HandLeft);
+            
+            return hand.Y < shoulder.Y + 0.1 &&  hand.Y > shoulder.Y - 0.1;
+        }
+
+        // -----------------------------
+        // Linkerarm omlaag /¨¨
+        // -----------------------------
+        public static bool LeftArmDown(KinectSkeleton skeleton)
+        {
+            var shoulder = skeleton.GetFrontViewInMeters(JointType.ShoulderLeft);
+            var hand = skeleton.GetFrontViewInMeters(JointType.HandLeft);
+            var elbow = skeleton.GetFrontViewInMeters(JointType.ElbowLeft);
+
+            return hand.Y < elbow.Y - 0.3 && elbow.Y < shoulder.Y + 0.1 && elbow.Y > shoulder.Y - 0.1;
+        }
+
+        // -----------------------------
+        // Rechterarm omhoog _/
+        // -----------------------------
+        public static bool RightArmUp(KinectSkeleton skeleton)
+        {
+            var shoulder = skeleton.GetFrontViewInMeters(JointType.ShoulderRight);
+            var elbow = skeleton.GetFrontViewInMeters(JointType.ElbowRight);
+            var hand = skeleton.GetFrontViewInMeters(JointType.HandRight);
+            
+            return hand.Y > elbow.Y + 0.3 && elbow.Y < shoulder.Y + 0.1 && elbow.Y > shoulder.Y - 0.1;
+        }
+
+        // -----------------------------
+        // Rechterarm opzij __
+        // -----------------------------
+        public static bool RightArmSide(KinectSkeleton skeleton)
+        {
+            var shoulder = skeleton.GetFrontViewInMeters(JointType.ShoulderRight);
+            var hand = skeleton.GetFrontViewInMeters(JointType.HandRight);
+            
+            return hand.Y < shoulder.Y + 0.1 &&  hand.Y > shoulder.Y - 0.1;
+        }
+
+        // -----------------------------
+        // Rechterarm omlaag ¨¨\
+        // -----------------------------
+        public static bool RightArmDown(KinectSkeleton skeleton)
+        {
+            var shoulder = skeleton.GetFrontViewInMeters(JointType.ShoulderRight);
+            var hand = skeleton.GetFrontViewInMeters(JointType.HandRight);
+            var elbow = skeleton.GetFrontViewInMeters(JointType.ElbowRight);
+
+            return hand.Y < elbow.Y - 0.3 && elbow.Y < shoulder.Y + 0.1 && elbow.Y > shoulder.Y - 0.1;
+        }
     }
 }
